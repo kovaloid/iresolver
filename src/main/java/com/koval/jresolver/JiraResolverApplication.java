2e9fe5d832945c18288e12e30d81ebbcfe65d1c5
kovala6 <kov3000@ya.ru>
2017-10-19 16:39:55 +0300

    Initial commit

---
+++ src/main/java/com/koval/jresolver/JiraResolverApplication.java
@@ -0,0 +1,12 @@
+package com.koval.jresolver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiraResolverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JiraResolverApplication.class, args);
+	}
+}
---
+++ src/test/java/com/koval/jresolver/JiraResolverApplicationTests.java
@@ -0,0 +1,16 @@
+package com.koval.jresolver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JiraResolverApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
