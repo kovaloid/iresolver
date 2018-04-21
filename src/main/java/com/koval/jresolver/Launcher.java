02f9a9020864a9ecd3bbdcacf723069f442c49bb
kovaloid <kov3000@ya.ru>
2018-04-21 16:22:59 +0300

    Modify dist

--- src/main/java/com/koval/jresolver/Launcher.java
+++ src/main/java/com/koval/jresolver/Launcher.java
@@ -1,45 +1,42 @@
 package com.koval.jresolver;
 
-import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
 
-import com.koval.jresolver.classifier.core.Classifier;
-import com.koval.jresolver.classifier.configuration.ClassifierProperties;
-import com.koval.jresolver.classifier.core.impl.DocClassifier;
-import com.koval.jresolver.connector.JiraConnector;
-import com.koval.jresolver.connector.configuration.JiraProperties;
-import com.koval.jresolver.report.core.impl.HtmlReportGenerator;
-import com.koval.jresolver.report.core.ReportGenerator;
-import com.koval.jresolver.rules.core.RuleEngine;
-import com.koval.jresolver.rules.core.impl.DroolsRuleEngine;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.core.io.support.ResourcePatternResolver;
 
+import com.koval.jresolver.classifier.configuration.ClassifierProperties;
+import com.koval.jresolver.classifier.core.Classifier;
+import com.koval.jresolver.classifier.core.impl.DocClassifier;
+import com.koval.jresolver.connector.JiraConnector;
+import com.koval.jresolver.connector.configuration.JiraProperties;
+import com.koval.jresolver.report.core.ReportGenerator;
+import com.koval.jresolver.report.core.impl.HtmlReportGenerator;
+import com.koval.jresolver.rules.core.impl.DroolsRuleEngine;
 
-public class Launcher {
+
+public final class Launcher {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(Launcher.class);
 
-  public static void main(String[] args) throws Exception {
+  private static Classifier classifier;
+  private static ReportGenerator reportGenerator;
 
-    //File f = new File("qwerty");
-    //f.mkdirs();
-    //System.exit(0);
-    //Enumeration<URL> e = Launcher.class.getClassLoader().getResources("placeholder");
-    //while (e.hasMoreElements()) {
-    //  System.out.println(e.nextElement());
-    //}
-    //System.out.println(checkDroolsFileExists());
-    //System.out.println(checkVectorModelFileExists());
-    //System.exit(0);
+  private Launcher() {
+  }
+
+  public static void main(String[] args) throws Exception {
+    ClassifierProperties classifierProperties = new ClassifierProperties("classifier.properties");
+    classifier = new DocClassifier(classifierProperties);
+    reportGenerator = new HtmlReportGenerator(classifier, new DroolsRuleEngine());
 
     if (args.length == 0) {
-      System.out.println("Use default argument 'run'");
+      LOGGER.info("Use default argument 'run'");
       run();
     } else if (args.length == 1) {
       switch (args[0]) {
@@ -53,51 +50,49 @@ public class Launcher {
           run();
           break;
         default:
-          System.out.println("Wrong arguments. Please use 'configure' or 'run'");
+          LOGGER.warn("Wrong arguments. Please use 'configure' or 'run'");
           break;
       }
     } else {
-      System.out.println("Too much arguments. Please use 'configure' or 'run'");
+      LOGGER.warn("Too much arguments. Please use 'configure' or 'run'");
     }
   }
 
   private static void prepare() throws URISyntaxException, IOException {
     LOGGER.info("Preparation...");
-    ClassifierProperties classifierProperties = new ClassifierProperties("classifier.properties");
-    Classifier classifier = new DocClassifier(classifierProperties);
     classifier.prepare();
   }
 
   private static void configure() throws URISyntaxException, IOException {
     LOGGER.info("Configuration...");
-    ClassifierProperties classifierProperties = new ClassifierProperties("classifier.properties");
-    Classifier classifier = new DocClassifier(classifierProperties);
+    if (checkVectorModelFileNotExists()) {
+      LOGGER.error("There're no VectorModel.zip dile");
+      return;
+    }
     classifier.configure();
-
-    File outputFolder = new File("../output");
-    outputFolder.mkdir();
+    reportGenerator.configure();
   }
 
   private static void run() throws Exception {
-    System.out.println("Generation...");
-    ClassifierProperties classifierProperties = new ClassifierProperties("classifier.properties");
-    Classifier classifier = new DocClassifier(classifierProperties);
-    RuleEngine ruleEngine = new DroolsRuleEngine();
-    ReportGenerator reportGenerator = new HtmlReportGenerator(classifier, ruleEngine);
+    LOGGER.info("Generation...");
+    if (checkDroolsFileNotExists()) {
+      LOGGER.error("There're no *.drl files");
+      return;
+    }
     JiraProperties jiraProperties = new JiraProperties("connector.properties");
     JiraConnector jiraConnector = new JiraConnector(jiraProperties);
     reportGenerator.generate(jiraConnector.getActualIssues());
   }
 
-  private static boolean checkVectorModelFileExists() {
+  private static boolean checkVectorModelFileNotExists() {
     URL vectorModelResource = Launcher.class.getClassLoader().getResource("VectorModel.zip");
-    return vectorModelResource != null;
+    return vectorModelResource == null;
   }
 
-  private static boolean checkDroolsFileExists() throws IOException {
+  private static boolean checkDroolsFileNotExists() throws IOException {
     ClassLoader classLoader = Thread.currentThread().getContextClassLoader().getClass().getClassLoader();
     ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver(classLoader);
     Resource[] resources = resolver.getResources("classpath*:*.drl");
-    return resources.length > 0;
+    return resources.length == 0;
   }
 }
