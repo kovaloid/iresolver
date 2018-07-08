abd1d04707ff11b65f64e05de64aecb79ff17275
AlexandraSokolova <37152165+AlexandraSokolova@users.noreply.github.com>
2018-07-08 11:32:20 +0400

    Revert "Added support for languages in stemmer"
    This reverts commit 7d24dab9411e351215a26bcb94ffed1576e74c81.

--- classifier/src/main/java/com/koval/jresolver/classifier/core/impl/StemmingPreprocessor.java
+++ classifier/src/main/java/com/koval/jresolver/classifier/core/impl/StemmingPreprocessor.java
@@ -1,29 +1,15 @@
 package com.koval.jresolver.classifier.core.impl;
 
 import org.deeplearning4j.text.tokenization.tokenizer.preprocessor.CommonPreprocessor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.tartarus.snowball.SnowballProgram;
-import org.tartarus.snowball.ext.*;
+import org.tartarus.snowball.ext.PorterStemmer;
 
-import com.koval.jresolver.classifier.configuration.ClassifierProperties;
 
 public class StemmingPreprocessor extends CommonPreprocessor {
-
-  private static final Logger LOGGER = LoggerFactory.getLogger(StemmingPreprocessor.class);
 
   @Override
   public String preProcess(String token) {
     String prep = super.preProcess(token);
-    SnowballProgram stemmer = new PorterStemmer();
-    try {
-      ClassifierProperties classifierProperties = new ClassifierProperties("classifier.properties");
-      Class stemmerClass = Class.forName("org.tartarus.snowball.ext."
-              + classifierProperties.getLanguage() + "Stemmer");
-      stemmer = (SnowballProgram)stemmerClass.newInstance();
-    } catch (Exception e) {
-      LOGGER.error("Could not find a stemmer", e);
-    }
+    PorterStemmer stemmer = new PorterStemmer();
     stemmer.setCurrent(prep);
     stemmer.stem();
     return stemmer.getCurrent();
