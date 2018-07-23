0226209cae4984446218211d99c5337e25854561
Maxim Solomonov <maxim.bannik@yandex.ru>
2018-07-24 00:03:48 +0300

    [WIP] Checkstyle fixes & RuleService improvements

--- rule-editor/backend/src/main/java/com/jresolver/editor/service/RuleService.java
+++ rule-editor/backend/src/main/java/com/jresolver/editor/service/RuleService.java
@@ -1,6 +1,7 @@
 package com.jresolver.editor.service;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 
 import org.slf4j.Logger;
@@ -16,7 +17,7 @@ public class RuleService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(RuleService.class);
 
-    private List<Rule> ruleList;
+    private final List<Rule> ruleList;
     private Integer maxId;
 
     public RuleService() throws IOException {
@@ -38,7 +39,7 @@ public class RuleService {
         List<Rule> rules = new LinkedList<>();
         Integer i = 0;
         for (final File file : RuleFinder.finder()) {
-            try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file), StandardCharsets.UTF_8))) {
                 String line = reader.readLine();
                 while (line != null) {
                     if ((line.length() > 4) && line.substring(0, 4).equals("rule")) {
@@ -72,7 +73,7 @@ public class RuleService {
                     }
                     line = reader.readLine();
                 }
-            } catch (Exception e) {
+            } catch (IOException e) {
                 e.printStackTrace();
             }
         }
@@ -95,19 +96,26 @@ public class RuleService {
 
     public Rule createRule(DraftRule payload) {
         LOGGER.info("Creating new rule");
-        return new Rule();
+        Rule rule = new Rule();
+        rule.setName(payload.getName());
+        rule.setFile(payload.getName() + ".drl");
+        rule.setConditions(payload.getConditions());
+        rule.setRecommendations(payload.getRecommendations());
+        rule.setId(++maxId);
+        ruleList.add(rule);
+        return rule;
     }
 
-    public void createRules(List<Rule> rules) throws IOException {
+    public void saveRules(List<Rule> rules) throws IOException {
         Map<String, LinkedList<Rule>> filelist = new HashMap<>();
         for (Rule rule : rules) {
             filelist.putIfAbsent(rule.getFile(), new LinkedList<>());
             filelist.get(rule.getFile()).add(rule);
         }
-        for (String filename : filelist.keySet()) {
-            List<Rule> rulesFromOneFile = filelist.get(filename);
+        for (Map.Entry<String, LinkedList<Rule>> entryList : filelist.entrySet()) {
+            List<Rule> rulesFromOneFile = entryList.getValue();
             File drl = new File(RuleFinder.getRulesDir(), rulesFromOneFile.get(0).getFile());
-            try (FileWriter writer = new FileWriter(drl)) {
+            try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(drl), StandardCharsets.UTF_8))) {
                 writer.write("package com.koval.jresolver.rules\n"
                         + "\n"
                         + "import com.koval.jresolver.connector.bean.JiraIssue;\n"
