c765a11a5fe98a38d1091f31b5214d93e339c818
Aemmie <alexshkr5@gmail.com>
2018-07-25 02:24:50 +0300

    some rule-editor service changes

---
+++ rule-editor/backend/src/main/java/com/jresolver/editor/bean/Metadata.java
@@ -0,0 +1,23 @@
+package com.jresolver.editor.bean;
+
+public class Metadata {
+  private String file;
+  private boolean modified;
+
+  public String getFile() {
+    return file;
+  }
+
+  public void setFile(String file) {
+    this.file = file;
+  }
+
+  public boolean isModified() {
+    return modified;
+  }
+
+  public void setModified(boolean modified) {
+    this.modified = modified;
+  }
+
+}
