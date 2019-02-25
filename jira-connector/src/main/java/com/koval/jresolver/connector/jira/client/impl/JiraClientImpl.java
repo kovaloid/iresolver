e64f78a7cd14a53ac0ec79f00f90db98bba860ae
kovaloid <kov3000@ya.ru>
2019-02-25 17:15:15 +0300

    Modify JiraClient

--- jira-connector/src/main/java/com/koval/jresolver/connector/jira/client/impl/JiraClientImpl.java
+++ jira-connector/src/main/java/com/koval/jresolver/connector/jira/client/impl/JiraClientImpl.java
@@ -2,6 +2,7 @@ package com.koval.jresolver.connector.jira.client.impl;
 
 import java.io.IOException;
 import java.util.HashSet;
+import java.util.Set;
 import java.util.function.Supplier;
 
 import org.slf4j.Logger;
@@ -28,28 +29,30 @@ public class JiraClientImpl implements JiraClient {
   @Override
   public SearchResult searchByJql(String jql, int maxResults, int startAt) {
     LOGGER.debug("Send search request: JQL = '{}' MaxResults = '{}' StartAt = '{}'.", jql, maxResults, startAt);
-    return checkAuthentication(() -> restClient.getSearchClient().searchJql(jql, maxResults, startAt, new HashSet<>()).claim(),
+    Set<String> fields = new HashSet<>();
+    fields.add("*all");
+    return checkRestExceptions(() -> restClient.getSearchClient().searchJql(jql, maxResults, startAt, fields).claim(),
         "Could not search by JQL.");
   }
 
   @Override
   public Issue getIssueByKey(String issueKey) {
     LOGGER.debug("Send issue request: IssueKey = '{}'.", issueKey);
-    return checkAuthentication(() -> restClient.getIssueClient().getIssue(issueKey).claim(),
+    return checkRestExceptions(() -> restClient.getIssueClient().getIssue(issueKey).claim(),
         "Could not get issue by key.");
   }
 
   @SuppressWarnings("PMD.PreserveStackTrace")
-  private <T> T checkAuthentication(Supplier<T> supplier, String message) {
+  private <T> T checkRestExceptions(Supplier<T> supplier, String message) {
     try {
       return supplier.get();
     } catch (RestClientException e) {
       e.getErrorCollections().forEach(errorCollection -> errorCollection.getErrorMessages().forEach(LOGGER::error));
       if (e.getStatusCode().isPresent() && e.getStatusCode().get().equals(400)) {
-        throw new JiraClientException("Bad Request.");
+        throw new JiraClientException("Bad Request. Incorrect JQL.");
       }
       if (e.getStatusCode().isPresent() && e.getStatusCode().get().equals(401)) {
-        throw new JiraClientException("Unauthorized.");
+        throw new JiraClientException("Unauthorized. Incorrect login or password.");
       }
       throw new JiraClientException(message, e);
     }
