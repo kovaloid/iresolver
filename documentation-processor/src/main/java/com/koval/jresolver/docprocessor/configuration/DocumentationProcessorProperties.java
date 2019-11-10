e7d7d8a038c44881def5774c1ed460be3e95a44f
kovaloid <kov3000@ya.ru>
2019-11-10 15:19:11 +0300

    Implement documentation processor (WIP)

--- documentation-processor/src/main/java/com/koval/jresolver/docprocessor/configuration/DocumentationProcessorProperties.java
+++ documentation-processor/src/main/java/com/koval/jresolver/docprocessor/configuration/DocumentationProcessorProperties.java
@@ -4,11 +4,18 @@ import com.koval.jresolver.common.api.doc2vec.Doc2VecProperties;
 
 public class DocumentationProcessorProperties extends Doc2VecProperties {
 
+  private String docsFolder;
+
   public DocumentationProcessorProperties() {
     super("documentation-processor.properties");
+    this.docsFolder = properties.getProperty("docsFolder");
   }
 
   public String getDocsFolder() {
-    return properties.getProperty("docsFolder");
+    return docsFolder;
+  }
+
+  public void setDocsFolder(String docsFolder) {
+    this.docsFolder = docsFolder;
   }
 }
--- src/main/java/com/koval/jresolver/util/LaunchUtil.java
+++ src/main/java/com/koval/jresolver/util/LaunchUtil.java
@@ -6,6 +6,9 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -14,9 +17,9 @@ import java.util.Objects;
 import com.koval.jresolver.common.api.doc2vec.VectorModel;
 import com.koval.jresolver.common.api.doc2vec.VectorModelCreator;
 import com.koval.jresolver.common.api.doc2vec.VectorModelSerializer;
+import com.koval.jresolver.common.api.util.TextUtil;
 import com.koval.jresolver.docprocessor.DocumentationProcessor;
 import com.koval.jresolver.docprocessor.configuration.DocumentationProcessorProperties;
-import com.koval.jresolver.docprocessor.core.DocDataSetCreator;
 import com.koval.jresolver.docprocessor.core.DocTypeDetector;
 import com.koval.jresolver.docprocessor.core.FileParser;
 import org.apache.commons.io.FileUtils;
@@ -96,24 +99,55 @@ public final class LaunchUtil {
   }
 
   public static void createDocumentationDataSet() {
-    // TODO: data sets creation
     DocumentationProcessorProperties properties = new DocumentationProcessorProperties();
-    DocDataSetCreator docDataSetCreator = new DocDataSetCreator();
     DocTypeDetector docTypeDetector = new DocTypeDetector();
     File docsFolder = new File(properties.getDocsFolder());
-    for (final File fileEntry: Objects.requireNonNull(docsFolder.listFiles())) {
-      if (fileEntry.isFile()) {
-        try (InputStream inputFileStream = new BufferedInputStream(new FileInputStream(fileEntry))) {
-          MediaType mediaType = docTypeDetector.detectType(inputFileStream, fileEntry.getName());
+
+    int pageIndex = 0;
+    int documentIndex = 0;
+
+    for (final File docFile : Objects.requireNonNull(docsFolder.listFiles())) {
+      if (docFile.isFile()) {
+        try (InputStream inputFileStream = new BufferedInputStream(new FileInputStream(docFile))) {
+          MediaType mediaType = docTypeDetector.detectType(inputFileStream, docFile.getName());
           if (docTypeDetector.isTypeSupported(mediaType)) {
-            FileParser fileParser = docTypeDetector.getFileParser(mediaType);
-            Map<Integer, String> result = fileParser.getMapping(inputFileStream);
-            docDataSetCreator.create(result, new File(properties.getWorkFolder(), properties.getDataSetFileName()));
+
+            try (PrintWriter dataSetOutput = new PrintWriter(new File(properties.getWorkFolder(), properties.getDataSetFileName()), StandardCharsets.UTF_8.name());
+                 PrintWriter metadataOutput = new PrintWriter(new File(properties.getWorkFolder(), "doc-metadata.txt"), StandardCharsets.UTF_8.name());
+                 PrintWriter docListOutput = new PrintWriter(new File(properties.getWorkFolder(), "doc-list.txt"), StandardCharsets.UTF_8.name())) {
+
+              FileParser fileParser = docTypeDetector.getFileParser(mediaType);
+              Map<Integer, String> result = fileParser.getMapping(inputFileStream);
+
+              for (Map.Entry<Integer, String> docPage : result.entrySet()) {
+                String docPageKey = "doc_" + pageIndex;
+                pageIndex++;
+
+                dataSetOutput.print(docPageKey);
+                dataSetOutput.print("|");
+                dataSetOutput.println(TextUtil.simplify(docPage.getValue()));
+
+                metadataOutput.print(docPageKey);
+                metadataOutput.print("|");
+                metadataOutput.print(documentIndex);
+                metadataOutput.print("|");
+                metadataOutput.println(docPage.getKey());
+              }
+
+              docListOutput.print(documentIndex);
+              docListOutput.print(" ");
+              docListOutput.println(docFile.getName());
+              documentIndex++;
+
+            } catch (FileNotFoundException | UnsupportedEncodingException e) {
+              LOGGER.error("Could not write to output file", e);
+            }
+
           }
         } catch (FileNotFoundException e) {
-          LOGGER.error("Could not find work folder", e);
+          LOGGER.error("Could not find documentation file: " + docFile.getAbsolutePath(), e);
         } catch (IOException e) {
-          LOGGER.error("Could not read files from work folder", e);
+          LOGGER.error("Could not read documentation file" + docFile.getAbsolutePath(), e);
         }
       }
     }
@@ -155,7 +189,7 @@ public final class LaunchUtil {
       Connector connector = getConnector(controlProperties, issueClient);
       IssueReceiver receiver = connector.getUnresolvedIssuesReceiver();
       ProcessExecutor executor = new ProcessExecutor();
-      for (IssueProcessor issueProcessor: getIssueProcessors(controlProperties, issueClient, ruleEngine)) {
+      for (IssueProcessor issueProcessor : getIssueProcessors(controlProperties, issueClient, ruleEngine)) {
         executor.add(issueProcessor);
       }
       if (controlProperties.isParallel()) {
@@ -171,7 +205,7 @@ public final class LaunchUtil {
       LOGGER.error("Could not run issues processing.", e);
     }
     try {
-      for (ReportGenerator reportGenerator: getReportGenerators(controlProperties)) {
+      for (ReportGenerator reportGenerator : getReportGenerators(controlProperties)) {
         reportGenerator.generate(results);
       }
     } catch (IOException e) {
@@ -248,7 +282,7 @@ public final class LaunchUtil {
       return new JiraConnector(issueClient, jiraConnectorProperties);
     } else if (ConnectorConstants.BUGZILLA.equalsIgnoreCase(connectorName)) {
       BugZillaConnectorProperties bugZillaConnectorProperties = new BugZillaConnectorProperties();
-      return  new BugZillaConnector(issueClient, bugZillaConnectorProperties);
+      return new BugZillaConnector(issueClient, bugZillaConnectorProperties);
     } else {
       throw new ResolverException("Could not get connector with name: " + connectorName);
     }
