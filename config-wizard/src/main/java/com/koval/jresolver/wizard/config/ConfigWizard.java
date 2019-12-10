a24cbaa3a76e1ed5e8ef7570a38e1d6267e10f87
kovaloid <kov3000@ya.ru>
2019-12-10 21:17:14 +0300

    Migrate from JavaFX to Swing in config wizard

--- config-wizard/src/main/java/com/koval/jresolver/wizard/config/ConfigWizard.java
+++ config-wizard/src/main/java/com/koval/jresolver/wizard/config/ConfigWizard.java
@@ -1,68 +1,87 @@
 package com.koval.jresolver.wizard.config;
 
+import java.awt.FlowLayout;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Enumeration;
-import java.util.List;
 import java.util.Properties;
 
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.FlowPane;
-import javafx.stage.Stage;
+import javax.swing.*;
 
 
-public class ConfigWizard extends Application {
+public final class ConfigWizard {
+
+  private ConfigWizard() {
+  }
 
   public static void main(String[] args) {
-    launch();
-  }
+    JFrame.setDefaultLookAndFeelDecorated(true);
 
-  @Override
-  public void start(Stage primaryStage) throws Exception {
+    JFrame frame = new JFrame("Configuration Wizard");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
     String javaVersion = System.getProperty("java.version");
     String javafxVersion = System.getProperty("javafx.version");
+    JLabel info = new JLabel("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
 
-    Label info = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+    JComboBox<String> options = new JComboBox<>();
+    options.addItem("jira");
+    options.addItem("bugzilla");
 
-    ComboBox<String> connector = new ComboBox<>();
-    connector.getItems().addAll("jira", "bugzilla");
+    JButton button = new JButton("Generate");
 
-    Button generateButton = new Button("Generate");
-    generateButton.addEventHandler(MouseEvent.MOUSE_CLICKED, mouseEvent -> {
-      createConfigurationFilesSet(connector.getValue());
+    button.addActionListener((actionEvent) -> {
+      System.out.println(actionEvent);
+      createConfigurationFilesSet((String)options.getSelectedItem());
     });
 
-    Scene scene = new Scene(new FlowPane(info, connector, generateButton), 640, 480);
+    frame.getContentPane().setLayout(new FlowLayout());
+    frame.getContentPane().add(info);
+    frame.getContentPane().add(button);
 
-    primaryStage.setTitle("Configuration Wizard");
-    primaryStage.setScene(scene);
-    primaryStage.show();
+    frame.pack();
+    frame.setLocationRelativeTo(null);
+    frame.setVisible(true);
+
+
+    frame.addWindowListener(new WindowListener() {
+      @Override
+      public void windowOpened(WindowEvent e) {
+      }
+
+      @Override
+      public void windowClosing(WindowEvent e) {
+        new File("control.properties").delete();
+        new File("jira-connector.properties").delete();
+      }
+
+      @Override
+      public void windowClosed(WindowEvent e) {
+      }
+
+      @Override
+      public void windowIconified(WindowEvent e) {
+      }
+
+      @Override
+      public void windowDeiconified(WindowEvent e) {
+      }
+
+      @Override
+      public void windowActivated(WindowEvent e) {
+      }
+
+      @Override
+      public void windowDeactivated(WindowEvent e) {
+      }
+    });
+
   }
 
-  @Override
-  public void stop() throws Exception {
-    new File("control.properties").delete();
-    new File("jira-connector.properties").delete();
-    // bugzilla-connector
-    // confluence-connector
-    // confluence-processor
-    // control
-    // documentation-processor
-    // html-reporter
-    // jira-connector
-    // similarity-processor
-  }
-
-  private void createConfigurationFilesSet(String option) {
+  private static void createConfigurationFilesSet(String option) {
     Properties control = new Properties();
     control.setProperty("connector", option);
     control.setProperty("processors", "value2");
@@ -76,12 +95,11 @@ public class ConfigWizard extends Application {
     createConfigurationFile("jira-connector.properties", "Jira properties file", jira);
   }
 
-  private void createConfigurationFile(String fileName, String description, Properties properties) {
+  private static void createConfigurationFile(String fileName, String description, Properties properties) {
     try (OutputStream output = new FileOutputStream(fileName)) {
       properties.store(output, description);
     } catch (IOException e) {
       e.printStackTrace();
     }
   }
-
 }
