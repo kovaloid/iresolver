19c9e80ed1a94bdc79e05aed0dd67c66aa2c37c2
kovaloid <kov3000@ya.ru>
2020-01-06 23:25:59 +0300

    Create metadata data set file in concluence connector

--- confluence-connector/src/main/java/com/koval/jresolver/connector/confluence/core/ConfluenceDataSetCreator.java
+++ confluence-connector/src/main/java/com/koval/jresolver/connector/confluence/core/ConfluenceDataSetCreator.java
@@ -6,29 +6,41 @@ import java.util.List;
 
 import com.atlassian.confluence.api.model.content.Content;
 import com.atlassian.confluence.api.model.content.ContentRepresentation;
+import com.koval.jresolver.common.api.util.TextUtil;
 
 
 public class ConfluenceDataSetCreator implements Closeable {
 
   private final PrintWriter dataFileOutput;
+  private final PrintWriter metadataFileOutput;
 
   public ConfluenceDataSetCreator() throws FileNotFoundException, UnsupportedEncodingException {
     dataFileOutput = new PrintWriter(new File("../data/", "ConfluenceDataSet.txt"), StandardCharsets.UTF_8.name());
+    metadataFileOutput = new PrintWriter(new File("../data/", "MetadataConfluenceDataSet.txt"), StandardCharsets.UTF_8.name());
   }
 
   public void handle(List<Content> results) {
     results.forEach((Content result) -> {
-      dataFileOutput.println("Page ID: " + result.getId().serialise());
-      dataFileOutput.println("Page Title: " + result.getTitle());
-      dataFileOutput.println("Page Body: " + result.getBody().get(ContentRepresentation.STORAGE).getValue()
-          .replace('\n', ' ')
-          .replaceAll("\\<.*?\\>", ""));
+      dataFileOutput.print(result.getId().serialise());
+      dataFileOutput.print("|");
+      String bodyWithoutTags = result.getBody()
+          .get(ContentRepresentation.STORAGE)
+          .getValue()
+          .replaceAll("nbsp", "")
+          .replaceAll("<.*?>", "");
+      dataFileOutput.println(TextUtil.simplify(bodyWithoutTags));
+
+      metadataFileOutput.print(result.getId().serialise());
+      metadataFileOutput.print(" ");
+      metadataFileOutput.println(result.getTitle());
     });
     dataFileOutput.flush();
+    metadataFileOutput.flush();
   }
 
   @Override
   public void close() throws IOException {
     dataFileOutput.close();
+    metadataFileOutput.close();
   }
 }
