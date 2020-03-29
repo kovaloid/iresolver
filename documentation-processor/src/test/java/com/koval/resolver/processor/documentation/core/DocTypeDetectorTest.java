b0a6dbf07c986572afa2a8dd1743a2dac66ce167
Daniil Shevtsov <me@daniilshevtsov.com>
2020-03-29 12:38:48 +0300

    Add the rest of tests for DocTypeDetectorTest

--- documentation-processor/src/test/java/com/koval/resolver/processor/documentation/core/DocTypeDetectorTest.java
+++ documentation-processor/src/test/java/com/koval/resolver/processor/documentation/core/DocTypeDetectorTest.java
@@ -8,12 +8,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class DocTypeDetectorTest {
+    private static final String FILE_NAME = "filename";
+
     private static final String PDF_EXTENSION = "pdf";
 
     private static final String DOC_EXTENSION = "doc";
     private static final String DOCX_EXTENSION = "docx";
 
-    private static final String INCORRECT_EXTENSION = "zzzz";
+    private static final String UNKNOWN_EXTENSION = "zzzz";
+
+    private static final String EXTENSION_DELIMITER = ".";
 
     private DocTypeDetector mDocTypeDetector;
 
@@ -29,9 +33,35 @@ class DocTypeDetectorTest {
 
     @Test
     void testDetectPdf() {
-        MediaType detectedType = mDocTypeDetector.detectType("." + PDF_EXTENSION);
+        MediaType detectedType = detectType(PDF_EXTENSION);
 
         assertEquals(MediaType.PDF, detectedType);
+    }
+
+    @Test
+    void testDetectWordFromDoc() {
+        MediaType detectedType = detectType(DOC_EXTENSION);
+
+        assertEquals(MediaType.WORD, detectedType);
+    }
+
+    @Test
+    void testDetectWordFromDocx() {
+        MediaType detectedType = detectType(DOCX_EXTENSION);
+
+        assertEquals(MediaType.WORD, detectedType);
+    }
+
+    @Test
+    void testDetectUnknownExtension() {
+        MediaType detectedType = detectType(UNKNOWN_EXTENSION);
+
+        assertEquals(MediaType.UNKNOWN, detectedType);
+    }
+
+    private MediaType detectType(String fileExtension) {
+        String fileName = FILE_NAME + EXTENSION_DELIMITER + fileExtension;
+        return mDocTypeDetector.detectType(fileName);
     }
 
 }
