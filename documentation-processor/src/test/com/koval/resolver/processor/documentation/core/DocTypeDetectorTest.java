1506d9043f8b4296370560dc217152e82c019730
Daniil Shevtsov <me@daniilshevtsov.com>
2020-03-29 12:14:40 +0300

    * Add Junit5 dependency to documentation-processor * Fix DOcTypeDetectorTest package location

--- documentation-processor/src/test/com/koval/resolver/processor/documentation/core/DocTypeDetectorTest.java
+++
@@ -1,7 +0,0 @@
-package com.koval.resolver.processor.documentation.core;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class DocTypeDetectorTest {
-
-}
---
+++ documentation-processor/src/test/java/com/koval/resolver/processor/documentation/core/DocTypeDetectorTest.java
@@ -0,0 +1,12 @@
+package com.koval.resolver.processor.documentation.core;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class DocTypeDetectorTest {
+    @Test
+    void testTrue() {
+        assertTrue(true);
+    }
+}
