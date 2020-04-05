b571aa34c00c5bcaee9e8bbbdd1ba24a4e516093
Daniil Shevtsov <me@daniilshevtsov.com>
2020-04-05 19:06:56 +0300

    Test DocFileDataParser in DocFileDataParserTest

--- documentation-processor/src/test/java/com/koval/resolver/processor/documentation/core/DocFileDataParserTest.java
+++ documentation-processor/src/test/java/com/koval/resolver/processor/documentation/core/DocFileDataParserTest.java
@@ -1,6 +1,5 @@
 package com.koval.resolver.processor.documentation.core;
 
-import com.koval.resolver.common.api.configuration.bean.processors.DocumentationProcessorConfiguration;
 import com.koval.resolver.processor.documentation.bean.DocFile;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -16,16 +15,16 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
-//TODO: This and other DocOutputFilesParser test will test different classes after refactoring
 @ExtendWith(MockitoExtension.class)
 public class DocFileDataParserTest {
   private static final String FILE_NAME = "FILE_NAME";
+  private static final String DELIMITER = " ";
 
   private static final DocFile DOC_FILE_1 = new DocFile(15, "filename1");
-  private static final String DOC_FILE_STRING_1 = "15 filename1";
+  private static final String DOC_FILE_STRING_1 = constructTestString(DOC_FILE_1);
 
   private static final DocFile DOC_FILE_2 = new DocFile(26, "filename2");
-  private static final String DOC_FILE_STRING_2 = "26 filename2";
+  private static final String DOC_FILE_STRING_2 = constructTestString(DOC_FILE_2);
 
   private static final String BOTH_FILES_STRING = DOC_FILE_STRING_1 + "\n" + DOC_FILE_STRING_2;
 
@@ -35,7 +34,7 @@ public class DocFileDataParserTest {
   @Mock
   private DocFileRepository mDocFileRepository;
 
-  private DocOutputFilesParser mDocOutputFilesParser;
+  private DocFileDataParser mDocFileDataParser;
 
   @BeforeEach
   void onSetup() throws IOException {
@@ -44,14 +43,14 @@ public class DocFileDataParserTest {
 
   @Test
   void testParsingOneLineDocMeta() {
-    DocFile actualDocFile = mDocOutputFilesParser.parseDocumentationFilesList().get(0);
+    DocFile actualDocFile = mDocFileDataParser.parseDocumentationFilesList().get(0);
 
     assertDocFilesEqual(DOC_FILE_1, actualDocFile);
   }
 
   @Test
   void testParsingMultipleDocMeta() {
-    List<DocFile> docFiles = mDocOutputFilesParser.parseDocumentationFilesList();
+    List<DocFile> docFiles = mDocFileDataParser.parseDocumentationFilesList();
 
     assertDocFilesEqual(DOC_FILE_1, docFiles.get(0));
     assertDocFilesEqual(DOC_FILE_2, docFiles.get(1));
@@ -62,7 +61,7 @@ public class DocFileDataParserTest {
   void testParsingInvalidStringOnlyChars() throws IOException {
     initDocOutputFileParser(INVALID_STRING_ONLY_CHARS);
 
-    mDocOutputFilesParser.parseDocumentationFilesList();
+    mDocFileDataParser.parseDocumentationFilesList();
   }
 
   //TODO: make this test pass
@@ -70,30 +69,26 @@ public class DocFileDataParserTest {
   void testParsingInvalidStringOnlyNumber() throws IOException {
     initDocOutputFileParser(INVALID_STRING_ONLY_NUMBER);
 
-    mDocOutputFilesParser.parseDocumentationFilesList();
+    mDocFileDataParser.parseDocumentationFilesList();
   }
 
   private void initDocOutputFileParser(String testString) throws IOException {
-    DocumentationProcessorConfiguration properties = createProperties(testString);
-
-    mDocOutputFilesParser = new DocOutputFilesParser(
-            properties,
-            mDocFileRepository
-    );
-  }
-
-  private DocumentationProcessorConfiguration createProperties(String testString) throws IOException {
     InputStream inputStream = new ByteArrayInputStream(testString.getBytes());
     when(mDocFileRepository.getFile(FILE_NAME)).thenReturn(inputStream);
 
-    DocumentationProcessorConfiguration properties = new DocumentationProcessorConfiguration();
-    properties.setDocsListFile(FILE_NAME);
-
-    return properties;
+    mDocFileDataParser = new DocFileDataParser(
+            FILE_NAME,
+            mDocFileRepository
+    );
   }
 
   private void assertDocFilesEqual(DocFile expectedDocFile, DocFile actualDocFile) {
     assertEquals(expectedDocFile.getFileIndex(), actualDocFile.getFileIndex());
     assertEquals(expectedDocFile.getFileName(), actualDocFile.getFileName());
   }
+
+  private static String constructTestString(DocFile docFile) {
+    return docFile.getFileIndex() + DELIMITER + docFile.getFileName();
+  }
+
 }
