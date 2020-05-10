ef067796f54f33d7c76e0943e3c157eea8602d46
Kirill Turchinskii <turchinskii.kirill@gmail.com>
2020-05-10 12:56:12 +0300

    Pull master file

--- src/main/java/com/koval/resolver/core/LauncherUI.java
+++ src/main/java/com/koval/resolver/core/LauncherUI.java
@@ -1,12 +1,12 @@
 package com.koval.resolver.core;
 
-import javax.swing.*;
 import java.awt.*;
-import java.util.logging.Logger;
+
+import javax.swing.*;
 
 
 class LauncherUI extends JFrame {
-  
+
   LauncherUI(Launcher launcher) {
     super("IResolver Application");
     initLookAndFeel();
@@ -46,12 +46,10 @@ class LauncherUI extends JFrame {
   }
 
   private void initLookAndFeel() {
-    final Logger logger = Logger.getLogger(LauncherUI.class.getName());
     JFrame.setDefaultLookAndFeelDecorated(true);
     try {
       UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-    } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e) {
-      logger.severe(e.getMessage());
+    } catch (Exception ignored) {
     }
   }
 }
