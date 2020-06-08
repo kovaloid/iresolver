270a61512a1c8e37513e70bd4fc5f73e44d4b457
Evgenii Timoshchuk <zheny.tim@gmail.com>
2020-06-08 10:20:00 +0300

    fix imports for conflict resovling

--- src/main/java/com/koval/resolver/core/Launcher.java
+++ src/main/java/com/koval/resolver/core/Launcher.java
@@ -3,6 +3,7 @@ package com.koval.resolver.core;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
