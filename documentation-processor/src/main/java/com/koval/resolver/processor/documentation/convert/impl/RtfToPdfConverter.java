165ac63931ed376787f8d5584b4acf6f47fbbba6
sophiacrumb <sophiacrumb@yandex.ru>
2021-04-20 12:48:19 +0300

    First commit to RTF-PDF Converter

---
+++ documentation-processor/src/main/java/com/koval/resolver/processor/documentation/convert/impl/RtfToPdfConverter.java
@@ -0,0 +1,57 @@
+package com.koval.resolver.processor.documentation.convert.impl;
+
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.Future;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.documents4j.api.IConverter;
+import com.documents4j.job.LocalConverter;
+import com.documents4j.api.DocumentType;
+import com.koval.resolver.processor.documentation.bean.MediaType;
+import com.koval.resolver.processor.documentation.convert.FileConverter;
+
+
+
+public class RtfToPdfConverter implements FileConverter {
+	private static final Logger LOGGER = LoggerFactory.getLogger(WordToPdfFileConverter.class);
+
+	@Override
+	public void convert(final String inputFilePath, final String outputFilePath) {
+		ByteArrayOutputStream bo = new ByteArrayOutputStream();
+		InputStream in = new BufferedInputStream (new FileInputStream(inputFilePath));
+		IConverter converter = LocalConverter.builder()
+				.workerPool(20, 25, 2, TimeUnit.SECONDS)
+				.processTimeout(5, TimeUnit.SECONDS)
+				.build();
+
+		Future<Boolean> conversion = converter
+				.convert(in).as(DocumentType.RTF)
+				.to(bo).as(DocumentType.PDF)
+				.prioritizeWith(1000)
+				.schedule();
+		conversion.get();
+		try (OutputStream outputStream = new FileOutputStream(outputFilePath)) {
+			bo.writeTo(outputStream);
+		} catch (IOException | InterruptedException | ExecutionException e) {
+			LOGGER.error("Could not convert rtf file " + inputFilePath + " to pdf " + outputFilePath, e);
+		}
+		in.close();
+		bo.close();
+	}
+
+	@Override
+	public MediaType getConvertibleType(MediaType docType) {
+		return MediaType.RTF;
+	}
+}
